846fafd553525ee32e8a9fb200ecf2855155af70
Coco <cocoxiao1998@gmail.com>
2019-10-28 00:23:21 -0400

    Watched dir has a corresponding hidden dir with the same path. Whatever is created in the watched dir is tracked. If it is a dir, then a hidden dir is created. If it is a file, then a hidden journal is created. Both with same paths in hidden dir. Need to add more metadata to journal rn. A lot of testing code too.

--- functions.py
+++ functions.py
@@ -1,23 +1,60 @@
-import os
+import os, errno
 import pyinotify
-
-wm = pyinotify.WatchManager()
-
-# the watched events
-mask = pyinotify.IN_CREATE
 
 # class that contains methods of events
 class EventHandler(pyinotify.ProcessEvent):
 	def process_IN_CREATE(self, event):
-		print("testing ...")
+		# testing purpose rn
+		print("Something was created")
+
+		# splitting path to get only name (might not need)
+		name = event.pathname.split('/')
+		name = name[-1]
+		print("File or dir name: ", name)
+
+		# checking if file type is .txt or if it is a dir
+		if os.path.isdir(event.pathname):
+			print("directory")
+			# will create dir in hidden dir
+			try:
+				dir = event.pathname.replace(watched_dir, watched_dir_hidden)
+				print("new hidden dir: ", dir)
+				os.makedirs(dir)
+			except:
+				print("already exists")
+				pass
+
+		if os.path.isfile(event.pathname):
+			print("checking if file ends in txt: ", event.pathname[-4:])
+			if event.pathname[-4:] == ".txt":
+				# will create a journal in the hidden dir
+				print("will create journal")
+				journal = event.pathname.replace(watched_dir, watched_dir_hidden)
+				journal = journal.replace(".txt", "-journal.txt")
+				f = open(journal, "a+")
+				f.write(name + "\n")
+				f.close()
+
+# the watched events (for now)
+mask = pyinotify.IN_CREATE
+
+# creating watch manager object
+wm = pyinotify.WatchManager()
+
+# adding the directory that will be watched and the watched events
+watched_dir = os.path.abspath("/home/coco/watched_dir")
+wm.add_watch(watched_dir, mask)
+
+# creating hidden dir with same path as watched dir
+watched_dir_hidden = "/home/coco/.watched_dir_hidden"
+try:
+	os.makedirs(watched_dir_hidden)
+except:
+	print("already exists")
+	pass
 
 # creating the notifier object
 notifier = pyinotify.Notifier(wm, EventHandler())
 
-# adding the directory that will be watched and the watched events
-watch_this = os.path.abspath("/home/coco/watched_dir")
-wm.add_watch(watch_this, mask)
-
 # starting the watch
 notifier.loop()
-
