d94c306c5d97b2d7d3e380b6eb981921bade7e1a
sammie10 <hsammie10@gmail.com>
2019-11-03 19:05:25 -0500

    Updated recovery.py

--- recovery.py
+++ recovery.py
@@ -1,26 +1,45 @@
-# user input file name to be read and written to
-uFile = open(raw_input("Enter filename: "), 'r')
-nFile = open(raw_input("Enter new filename: "), 'a+')
 
-# Set variables to traverse the text file
-lines = uFile.readlines()
-changes = 8
-result = []
+#def process_lineNum():
 
-# Get the changes column and append to array
-for x in lines:
-    result.append(x.split()[changes])
+#def process_mod():
 
-uFile.close() # Close the file
+'''
+process_changes function gets the changes that were made 
+and write into new text file
+'''
 
-# Get the changes that were made and write into new text file
-length = len(result)
+def process_changes(changesA):
+    length = len(changesA)
+    #cResult = []
 
-for i in range(0, length):
-    s = result[i]
-    slen = len(s)
-    start = s.find("\'")
-    nS = s[start + 1:-2]
-    nFile.write(nS + "\n")
+    for i in range(0, length):
+        s = changesA[i]
+        slen = len(s)
+        start = s.find("\'")
+        nS = s[start + 1:-2]
+        nFile.write(nS + "\n")
+        print("\nFile re-creation complete!")
 
-print("\nFile re-creation complete!")
+if __name__ == "__main__":
+    # user input file name to be read and written to
+    uFile = open(raw_input("Enter filename: "), 'r')
+    nFile = open(raw_input("Enter new filename: "), 'a+')
+    lines = uFile.readlines()
+    
+    lineNum = 8
+    mod = 9
+    changes = 10
+
+    lineNumA = []
+    modA = []
+    changesA = []
+
+    # Get the line number column and append to array
+    for x in lines:
+        lineNumA.append(x.split()[lineNum])
+        modA.append(x.split()[mod])
+        changesA.append(x.split()[changes])
+
+    process_changes(changesA)
+
+    uFile.close() # Close the file
