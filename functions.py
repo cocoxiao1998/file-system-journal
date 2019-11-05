744c6c3a250bd85b5119374b4ac735cb7d997630
Coco <cocoxiao1998@gmail.com>
2019-11-05 16:31:32 -0500

    Worked on adding lines in journal entry in mod function. Still need to work on deleting lines.

--- functions.py
+++ functions.py
@@ -3,11 +3,7 @@ import errno
 import stat
 import pyinotify
 import time
-<<<<<<< HEAD
 import difflib
-=======
-import datetime
->>>>>>> 187efd138681b97907649cb1d2194297c0f5058d
 
 # class that contains methods of events
 class EventHandler(pyinotify.ProcessEvent):
@@ -15,14 +11,7 @@ class EventHandler(pyinotify.ProcessEvent):
 		# check that it is a text file that is being created
 		if os.path.isfile(event.pathname):
 			if event.pathname[-4:] == ".txt":
-<<<<<<< HEAD
-=======
-				# will create a journal in the hidden dir
-				journal = event.pathname.replace(watched_dir, watched_dir_hidden)
-				journal = journal.replace(".txt", "-journal.txt")
-				f = open(journal, "a+")
-				
->>>>>>> 187efd138681b97907649cb1d2194297c0f5058d
+
 				# setting the metadata in variables first
 				# inode number
 				inode = os.lstat(event.pathname)[stat.ST_INO]
@@ -72,7 +61,6 @@ class EventHandler(pyinotify.ProcessEvent):
 
 				# timestamp
 				timestamp = time.ctime(os.path.getmtime(event.pathname))
-				print("timestamp: ", timestamp)
 
 
 				# getting changes
@@ -102,7 +90,7 @@ class EventHandler(pyinotify.ProcessEvent):
 					# checking if line is unique to file being modified
 					if operator == "-":
 						# journal entry: removing a line
-						#change = "(" + str(line_num) + " - '" +
+						change = "(" + str(line_num) + " - '" + "')" 
 
 						# journal entry: adding a line
 						line = line[2:]
@@ -114,76 +102,25 @@ class EventHandler(pyinotify.ProcessEvent):
 
 				# closing files
 				f.close()
-<<<<<<< HEAD
 				h.close()
 				j.close()
 
 				# updating hidden file
 
-# the watched events (for now)
-mask = pyinotify.IN_CREATE | pyinotify.IN_MODIFY
-=======
-	def process_IN_DELETE(self, event): #if any file or folder within the directory is observed being deleted
-		
-		#get name of file and extension just deleted
-		name = os.path.basename(event.pathname)
 
-		#if the filename is of the .txt extension
-		if name[-4:] == ".txt":
-			
-			#editing the hidden file... more to be done on this later
-			#BELOW MUST BE UPDATED UPON IMP OF THE SAME METHODS IN ON_CREATE HIDDEN_FILE CREATION
-			hidden_file = event.pathname.replace(watched_dir, watched_dir_hidden)
-			hidden_file = hidden_file.replace(".txt", "-hidden-file.txt")
-			h = open(hidden_file, "a+")
-			h.close()
-			#ABOVE MUST BE UPDATED UPON IMP OF THE SAME METHODS IN ON_CREATE HIDDEN_FILE CREATION
-
-			#from original ON_CREATE allowing user to open the filename-journal.txt to show changes
-			#journal = event.pathname.replace(watched_dir, watched_dir_hidden)
-			journal = watched_dir + "/.watched_dir_hidden/" + name
-			journal = journal.replace(".txt", "-journal.txt")
-
-			#open file journal explicitly for reading
-			f = open(journal, "r")
-			#since the file is deleted the below code will get the information about the file from the file journal
-			contents = f.readlines()[-1] #read only the last line of the journal for information
-			node = contents.split(' ',1)[0] #extracts the inode id of the file just deleted
-			permissions = contents.split(' ', 3)[2] #extracts the permissions of the file just deleted
-			now = datetime.datetime.now() #records current time to variable
-			
-			#formatting the day to not contain a zero if the day is a single digit
-			#please watch this part of the code
-			day = now.strftime("%d")
-			if day[0] == '0':
-				day = day[1]
-			#formatting the timestamp to match the ON_CREATE situation
-			timestamp = now.strftime("%a %b  " + day +" %H:%M:%S %Y")
-
-			#closing the file for read-only purposes
-			f.close()
-
-			#open file journal explicitly for appending
-			f = open(journal, "a+")
-			#formatting write output
-			f.write(node + " " + name + " " + permissions + " " + timestamp + " (~)\n")
-
-			#close the file
-			f.close()
 
 # the watched events (for now), add each event as you work on it
-mask = pyinotify.IN_CREATE | pyinotify.IN_DELETE
->>>>>>> 187efd138681b97907649cb1d2194297c0f5058d
+mask = pyinotify.IN_CREATE | pyinotify.IN_MODIFY
 
 # creating watch manager object
 wm = pyinotify.WatchManager()
 
 # adding the directory that will be watched and the watched events
-watched_dir = os.path.abspath("/home/peter/Desktop/TEST")
+watched_dir = os.path.abspath("/home/coco/watched_dir")
 wm.add_watch(watched_dir, mask)
 
 # creating hidden dir with same path as watched dir
-watched_dir_hidden = "/home/peter/Desktop/TEST/.watched_dir_hidden"
+watched_dir_hidden = "/home/coco/.watched_dir_hidden"
 try:
 	os.makedirs(watched_dir_hidden)
 except:
