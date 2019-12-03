7c92584ca47448529af02718408294b06471c37b
fredoniaPixel <stas0391@fredonia.edu>
2019-12-03 08:55:15 -0500

    made some format changes

--- functions.py
+++ functions.py
@@ -4,12 +4,13 @@ import stat
 import pyinotify
 import time
 import datetime
+import difflib
 
 # class that contains methods of events
 class EventHandler(pyinotify.ProcessEvent):
 	def process_IN_CREATE(self, event):
-
-		# check if whatever is created is  a file, and then a text file
+		
+		# check if whatever is created is a file and if it is a .txt file
 		if os.path.isfile(event.pathname):
 			if event.pathname[-4:] == ".txt":
 
@@ -24,8 +25,16 @@ class EventHandler(pyinotify.ProcessEvent):
 				permissions = os.stat(event.pathname)[stat.ST_MODE]
 				permissions = oct(permissions)[-3:]
 
-				# timestamp
-				timestamp = time.ctime(os.path.getctime(event.pathname))
+				#BEGIN BELOW CHANGES
+				# timestamp #the below was replaced 11/23/19 to help confirm ON_CREATE wasn't calling IN_ATTRIB - PETE
+				now = datetime.datetime.now()
+				day = now.strftime("%d")
+				if day[0] == '0':
+					day = day[1]
+				timestamp = now.strftime("%a %b " + day +" %H:%M:%S:%f %Y")
+				#the original timestamp is below
+				#timestamp = time.ctime(os.path.getctime(event.pathname)) #previous method replaced with above to avoid CREATE and ATTRIB collisions
+				#END ABOVE CHANGES
 
 				# getting change: will set the changes to "line #, +/-, content"
 				file = event.pathname
@@ -54,6 +63,8 @@ class EventHandler(pyinotify.ProcessEvent):
 				for line in change:
 					j.write(str(inode) + " " + name + " " + str(permissions) + " " + timestamp + " ('" + line + "')\n")
 				j.close()
+				#print(timestamp)
+		#print("called")
 
 	def process_IN_DELETE(self, event): #if any file or folder within the directory is observed being deleted
 
@@ -63,59 +74,65 @@ class EventHandler(pyinotify.ProcessEvent):
 		#if the filename is of the .txt extension
 		if name[-4:] == ".txt":
 
-			#deleting the hidden file to allow proper reuse of the filename
+			#indicating what the name of the hidden file should be
 			hidden_file = event.pathname.replace(watched_dir, watched_dir_hidden)
 			hidden_file = hidden_file.replace(".txt", "-hidden-file.txt")
-			#remove the hidden file
-			os.remove(hidden_file)
 
-			#from original ON_CREATE allowing user to open the filename-journal.txt to show changes
-			journal = event.pathname.replace(watched_dir, watched_dir_hidden)
-			journal = journal.replace(".txt", "-journal.txt")
+			#if the file journal and file copy both exist, delete the hidden_file and edit the journal
+			if os.path.isfile(hidden_file):
+				os.remove(hidden_file) #remove the hidden file
 
-			#open file journal explicitly for reading
-			f = open(journal, "r")
-			#since the file is deleted the below code will get the information about the file from the file journal
-			contents = f.readlines()[-1] #read only the last line of the journal for information
-			node = contents.split(' ',1)[0] #extracts the inode id of the file just deleted
-			permissions = contents.split(' ', 3)[2] #extracts the permissions of the file just deleted
-			now = datetime.datetime.now() #records current time to variable
+				#from original ON_CREATE allowing user to open the filename-journal.txt to show changes
+				journal = event.pathname.replace(watched_dir, watched_dir_hidden)
+				journal = journal.replace(".txt", "-journal.txt")
 
-			#formatting the day to not contain a zero if the day is a single digit
-			#please watch this part of the code
-			day = now.strftime("%d")
-			if day[0] == '0':
-				day = day[1]
-			#formatting the timestamp to match the ON_CREATE situation
-			timestamp = now.strftime("%a %b  " + day +" %H:%M:%S %Y")
+				#open file journal explicitly for reading
+				f = open(journal, "r")
+				#since the file is deleted the below code will get the information about the file from the file journal
+				contents = f.readlines()[-1] #read only the last line of the journal for information
+				node = contents.split(' ',1)[0] #extracts the inode id of the file just deleted
+				permissions = contents.split(' ', 3)[2] #extracts the permissions of the file just deleted
+				now = datetime.datetime.now() #records current time to variable
 
-			#closing the file for read-only purposes
-			f.close()
+				#formatting the day to not contain a zero if the day is a single digit
+				#please watch this part of the code
+				day = now.strftime("%d")
+				if day[0] == '0':
+					day = day[1]
+				#formatting the timestamp to match the ON_CREATE situation
+				timestamp = now.strftime("%a %b " + day +" %H:%M:%S:%f %Y")
 
-			#open file journal explicitly for appending
-			f = open(journal, "a+")
-			#formatting write output
-			f.write(node + " " + name + " " + permissions + " " + timestamp + " (~)\n")
+				#closing the file for read-only purposes
+				f.close()
 
-			#close the file
-			f.close()
+				#open file journal explicitly for appending
+				f = open(journal, "a+")
+				#formatting write output
+				f.write(node + " " + name + " " + permissions + " " + timestamp + " (~)\n")
+
+				#close the file
+				f.close()
+			else:
+				print("Filename \"" + name + "\" was detected as being removed from the below watched directory: \n" + watched_dir + ".\nNo Journal or Hidden Copy was found for this file, thus no changes have been recorded.\nPlease ensure the script is running before working in your watched directory.")
 
 # the watched events (for now), add each event as you work on it
-mask = pyinotify.IN_CREATE | pyinotify.IN_DELETE
+mask = pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_ATTRIB | pyinotify.IN_MOVED_TO | pyinotify.IN_MOVED_FROM
 
 # creating watch manager object
 wm = pyinotify.WatchManager()
 
-# adding the directory that will be watched and the watched events
-watched_dir = os.path.abspath("/home/coco/watched_dir")
-wm.add_watch(watched_dir, mask)
+#saving the file directory names that will be watched to variables
+watched_dir = os.path.abspath("Desktop/watched_dir")
+watched_dir_hidden = "Desktop/watched_dir/.watched_dir_hidden"
 
-# creating hidden dir with same path as watched dir
-watched_dir_hidden = "/home/coco/.watched_dir_hidden"
+#if the above files have not yet been created, do so:
 try:
-	os.makedirs(watched_dir_hidden)
+	os.makedirs(watched_dir_hidden)#makedirs makes all parent directories automatically
 except:
 	pass
+
+#adding the watched directory so functions can be invoked by Create, Delete, and Modify detections
+wm.add_watch(watched_dir, mask)
 
 # creating the notifier object
 notifier = pyinotify.Notifier(wm, EventHandler())
